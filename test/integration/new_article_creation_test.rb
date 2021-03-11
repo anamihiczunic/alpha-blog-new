f99915be985a3984965ce7946e0ad40b1992237e
Ana Mihic Zunic <anamihiczunic@gmail.com>
2021-03-11 15:42:41 +0100

    Final version

--- test/integration/new_article_creation_test.rb
+++ test/integration/new_article_creation_test.rb
@@ -1,19 +1,18 @@
 require "test_helper"
 
-class CreateCategoryTest < ActionDispatch::IntegrationTest
-
+class NewArticleCreationTest < ActionDispatch::IntegrationTest
   setup do
     @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
                               password: "password", admin: true)
     sign_in_as(@admin_user)
   end
 
-  test "get new category form and create category" do
+  test "get new article form and create article" do
 
-    get "/categories/new"
+    get "/articles/new"
     assert_response :success
-    assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: 'Sports'} }
+    assert_difference 'Article.count', 1 do
+      post articles_path, params: { article: { title: 'Sports', description: "some description"} }
       assert_response :redirect
     end
     follow_redirect!
@@ -22,15 +21,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match "Sports", response.body
   end
 
-  test "get new category form and reject invalid category submission" do
-    get "/categories/new"
+  test "get new article form and reject article submission with invalid title" do
+
+    get "/articles/new"
     assert_response :success
-    assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: ' '} }
+    assert_no_difference 'Article.count' do
+      post articles_path, params: { article: { title: 'S', description: "some description"} }
     end
-    # we are looking for a word "errors" in html body of html page that is shown when we are being renderd to a new category page
     assert_match "errors", response.body
     assert_select 'div.alert'
     assert_select 'h4.alert-heading'
+
   end
+
+
 end
